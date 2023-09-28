b3f82e8ac88475044d88b973051ed7203119fa9f
alex <alex.mayorov@alfabit.org>
2023-09-28 21:15:53 +0300

    rewrite autoredirect tests by using unittest

--- tests/autoredirect.py
+++ tests/autoredirect.py
@@ -1,3 +1,5 @@
+import unittest
+
 from misc.head import Head
 from misc.status import Status
 from wires.autoredirect import AutoRedirect
@@ -8,10 +10,11 @@ class RedirectWire:
         return "HTTP/1.1 301 Moved Permanently\r\nLocation: https://www.google.com/\r\n\r\n"
 
 
-res_head = Head(AutoRedirect(RedirectWire()).send("\r\n".join([
-    "GET / HTTP/1.1", "Host: www.google.com",
-    "Connection: Close\r\n\r\n"
-])))
-
-assert Status(res_head).int_value() == 200
-assert 'google.com' in res_head.value()
+class TestAutoRedirect(unittest.TestCase):
+    def test_redirect(self):
+        res_head = Head(AutoRedirect(RedirectWire()).send("\r\n".join([
+            "GET / HTTP/1.1", "Host: www.google.com",
+            "Connection: Close\r\n\r\n"
+        ])))
+        self.assertEqual(Status(res_head).int_value(), 200)
+        self.assertIn('google.com', res_head.value())
