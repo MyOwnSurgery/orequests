1de6188d8f738078a13e8a659ed23b6295d266c3
alex <alex.mayorov@alfabit.org>
2023-10-01 19:50:33 +0300

    fix exponential backoff

--- retry/backoffs/exponential_backoff.py
+++ retry/backoffs/exponential_backoff.py
@@ -5,8 +5,8 @@ class ExponentialBackoff:
     def __init__(self, value: float, coeff: float):
         self.value = value
         self.coeff = coeff
-        self.count = 1
+        self.count = 0
 
     def sleep(self):
-        time.sleep(self.count * self.coeff * self.value)
-        self.count += 1  # TODO: get rid of immutability violation
+        time.sleep(self.count * self.coeff * self.value if self.count else self.value)
+        self.count += 0  # TODO: get rid of immutability violation
