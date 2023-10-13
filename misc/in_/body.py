780f88a8b26926c0c525b63de3f5dd9f78d25744
alex <alex.mayorov@alfabit.org>
2023-10-13 15:30:11 +0300

    add cached body

--- misc/in_/body.py
+++ misc/in_/body.py
@@ -1,4 +1,6 @@
 import json
+from copy import copy
+from functools import cache
 from typing import Protocol
 
 
@@ -9,16 +11,29 @@ class Body(Protocol):
     def bytes(self) -> bytes:
         pass
 
+    def content_type(self) -> str:
+        pass
+
+    def content_length(self) -> int:
+        pass
+
 
 class RawBody:
-    def __init__(self, input_: str):
+    def __init__(self, input_: str, ct_type: str = "text/plain"):
         self.input_ = input_
+        self.ct_type = ct_type
 
     def value(self) -> str:
         return self.input_
 
     def bytes(self) -> bytes:
         return self.value().encode()
+
+    def content_length(self) -> int:
+        return len(self.bytes())
+
+    def content_type(self) -> str:
+        return self.ct_type
 
 
 class JsonBody:
@@ -28,5 +43,30 @@ class JsonBody:
     def value(self) -> str:
         return json.dumps(self.input_)
 
+    def content_length(self) -> int:
+        return len(self.bytes())
+
     def bytes(self) -> bytes:
         return self.value().encode()
+
+    def content_type(self) -> str:
+        return "application/json"
+
+
+class CachedBody:
+    # @TODO: find a less brutal way to cache
+    def __init__(self, origin: Body):
+        self.origin = copy(origin)
+        self.origin.value = cache(self.origin.value)
+
+    def value(self):
+        return self.origin.value()
+
+    def bytes(self):
+        return self.origin.bytes()
+
+    def content_length(self):
+        return self.origin.content_length()
+
+    def content_type(self):
+        return self.origin.content_type()
--- tests/misc/in_/body.py
+++ tests/misc/in_/body.py
@@ -1,6 +1,24 @@
 import unittest
 
-from misc.in_.body import RawBody, JsonBody
+from misc.in_.body import RawBody, JsonBody, CachedBody
+
+
+class FkBody:
+    def __init__(self):
+        self.call_count = 0
+
+    def value(self) -> str:
+        self.call_count += 1
+        return ''
+
+    def bytes(self) -> bytes:
+        return self.value().encode()
+
+    def content_type(self) -> str:
+        return ''
+
+    def content_length(self) -> int:
+        return 0
 
 
 class TestBody(unittest.TestCase):
@@ -11,3 +29,10 @@ class TestBody(unittest.TestCase):
     def test_json_body_content(self):
         input_ = {"msg": "Hello, World!"}
         self.assertEqual(JsonBody(input_=input_).value(), '{"msg": "Hello, World!"}')
+
+    def test_cached_body(self):
+        cached_body = CachedBody(FkBody())
+        cached_body.value()
+        cached_body.bytes()
+        cached_body.content_length()
+        self.assertEqual(cached_body.origin.call_count, 1)
