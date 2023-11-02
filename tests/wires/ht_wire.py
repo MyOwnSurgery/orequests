6c44ca35b64107807f01b6693230906888834d38
alex <alex.mayorov@alfabit.org>
2023-11-02 22:37:01 +0300

    rename htwire test

--- tests/wires/ht_wire.py
+++ tests/wires/ht_wire.py
@@ -21,7 +21,7 @@ class FkConnection:
         return self.response
 
 
-class TestTimedWire(unittest.TestCase):
+class TestHtWire(unittest.TestCase):
     def test_response_simple(self):
         response = 'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 16\r\n\r\n{"msg": "HELLO"}'
         with FkConnection(response.encode()) as conn:
