39675bbdc863a6f7b011bde13ba6a922ab43bb21
alex <alex.mayorov@alfabit.org>
2023-11-28 12:48:24 +0300

    reformat code

--- wires/autoredirect.py
+++ wires/autoredirect.py
@@ -38,17 +38,15 @@ class AutoRedirect:
             new_request = Request(
                 st_line=start_line,
                 headers=headers,
-                body=RawBody(
-                    new_body, ct_type=headers["Content-Type"]
-                ) if new_body else None,
+                body=RawBody(new_body, ct_type=headers["Content-Type"])
+                if new_body
+                else None,
             )
 
             is_safe = scheme == "https"
             if not port:
                 port = 443 if is_safe else 80
-            new_wire = (
-                SafeWire(host, port) if is_safe else Wire(host, port)
-            )
+            new_wire = SafeWire(host, port) if is_safe else Wire(host, port)
 
             res = new_wire.send(new_request)
             head = Head(res)
