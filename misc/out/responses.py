854ea940370ea50db0416a48611f4b3c13c32d28
alex <alex.mayorov@alfabit.org>
2023-11-28 12:26:08 +0300

    delete responses class

--- misc/out/responses.py
+++
@@ -1,6 +0,0 @@
-class Responses:
-    def __init__(self, input_: str):
-        self.input_ = input_
-
-    def value(self) -> list[str]:
-        return [x for x in self.input_.split("\r\n0\r\n\r\n") if x]
